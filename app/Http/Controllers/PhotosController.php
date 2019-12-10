1710eed0944557201cb1fa0b287bf8be9b632e0e
Anderson <ander-reis@hotmail.com>
2019-12-10 08:08:06 -0300

    Initial commit

---
+++ app/Http/Controllers/PhotosController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Photo;
+use Illuminate\Http\Request;
+
+class PhotosController extends Controller
+{
+    public function create()
+    {
+        return view('photos.create');
+    }
+
+    public function store(Request $request)
+    {
+        $file = $request->file('url');
+        $validExtensions = ['jpeg', 'jpg', 'png', 'gif'];
+        if (!is_null($file) and $file->isValid() and in_array($file->extension(), $validExtensions)) {
+            $name = $file->getClientOriginalName();
+            $data = $request->all();
+            $file->storeAs('img', $name);
+            $data['url'] = $name;
+            Photo::create($data);
+//            Photo::create($request->all());
+            dd('salvo com sucesso');
+        }
+        dd('inválido');
+    }
+}
